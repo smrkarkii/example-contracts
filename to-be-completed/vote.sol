d9f2c49d36f86135b1ea2c6690e8e8287f151180
smrkarkii <076bct080.smriti@pcampus.edu.np>
2022-09-17 13:11:53 +0545

    completed upto voting

--- to-be-completed/vote.sol
+++ to-be-completed/vote.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier:MIT
 pragma solidity ^0.6.4;
 
 contract cityPoll {
@@ -9,8 +10,8 @@ contract cityPoll {
     }
 
 
-    mapping() public cities; //mapping city Id with the City ctruct - cityId should be uint256
-    mapping() hasVoted; //mapping to check if the address/account has voted or not
+    mapping(uint256 => City) public cities; //mapping city Id with the City ctruct - cityId should be uint256
+    mapping(address => bool) hasVoted; //mapping to check if the address/account has voted or not
 
     address owner;
     uint256 public cityCount = 0; // number of city added
@@ -18,23 +19,38 @@ contract cityPoll {
     
     //TODO set contract caller as owner
     //TODO set some intitial cities.
+    owner = msg.sender;
+    cities[0].cityName = "Ilam";
+    cities[1].cityName = "Kathmandu";
+    cities[2].cityName = "Bhaktapur";
+    cityCount = 3;
+    
+    
+
     }
  
  
-    function addCity() public {
+    function addCity(uint256 cityId, string memory name) public {
       //  TODO: add city to the CityStruct
-
+        cities[cityId].cityName = name;
+        cityCount++;
     }
     
-    function vote() public {
+    function vote(uint256 cityId) public {
         
         //TODO Vote the selected city through cityID
+        require(!hasVoted[msg.sender], "Already voted");
+        cities[cityId].vote++;
+        hasVoted[msg.sender] = true;
+        }
+
+    function getCity(uint cityId) public view returns (string memory) {
+     // TODO get the city details through cityID
+     return cities[cityId].cityName;
 
     }
-    function getCity() public view returns (string memory) {
-     // TODO get the city details through cityID
-    }
-    function getVote() public view returns (uint256) {
+    function getVote(uint cityId) public view returns (uint256) {
     // TODO get the vote of the city with its ID
+    return cities[cityId].vote;
     }
 }
